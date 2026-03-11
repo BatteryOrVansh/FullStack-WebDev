032a0819d8bab2d101c68e638049d36ae534593f
Vansh Dixit <officialvanshdixit@gmail.com>
2026-03-11 22:26:24 +0530

    add functions 5

--- JS-Challenges/FUNCTIONS.js
+++ JS-Challenges/FUNCTIONS.js
@@ -51,4 +51,11 @@ console.log(result);
 5. Write a function named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`. 
 Store the returned function in a variable named `teaMaker` and call it with `"green tea"`.
 */
+function createTeaMaker(){
+    return function(teaType) {
+        return `Making ${teaType}`;
+    }
+}
+teaMaker = createTeaMaker();
+console.log(teaMaker("green tea"));
 
