ba159e933058c1ec860b14f4fb712508041f8b0f
Vansh Dixit <officialvanshdixit@gmail.com>
2026-03-11 15:58:26 +0530

    add functions 2

--- JS-Challenges/FUNCTIONS.js
+++ JS-Challenges/FUNCTIONS.js
@@ -13,6 +13,13 @@ console.log(teaOrder);
 2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
 Call `confirmOrder` from within `orderTea` and return the result.
 */
+function orderTea(teaType){
+    function confirmOrder(){
+        return `Order confirmed for chai`;
+    }
+    return confirmOrder();
+}
+console.log(orderTea("masala chai"));
 
 
 
