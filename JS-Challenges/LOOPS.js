2f3dd7288f6d59017ff4436dd290fef4d889fbc6
Vansh Dixit <officialvanshdixit@gmail.com>
2026-03-11 13:22:01 +0530

    add loops-9

--- JS-Challenges/LOOPS.js
+++ JS-Challenges/LOOPS.js
@@ -109,6 +109,15 @@ console.log(visitedCities);
 9. Use a `for-of` loop to iterate through the array `[1, 2, 3, 4, 5]` and stop when the number `4` is found. 
    Store the numbers before `4` in an array named `smallNumbers`.
 */
+let narray = [1, 2, 3, 4, 5];
+let smallNumbers = [];
+for (let num of narray) {
+    if (num == 4) {
+        break
+    }
+    smallNumbers.push(num)
+}
+console.log(smallNumbers);
 
 
 /* 
