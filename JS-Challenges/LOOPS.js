5f15761681aa30c6562e7f74751308edaf76789c
Vansh Dixit <officialvanshdixit@gmail.com>
2026-03-11 14:55:09 +0530

    add loops-12

--- JS-Challenges/LOOPS.js
+++ JS-Challenges/LOOPS.js
@@ -174,8 +174,22 @@ let worldCities = {
     "Berlin": 3500000,
     "Paris": 2200000
 };
+*/
+let worldCities = {
+    "Sydney": 5000000,
+    "Tokyo": 9000000,
+    "Berlin": 3500000,
+    "Paris": 2200000
+};
+let largeCities = {};
+for (let city in worldCities) {
+    if (worldCities[city]<3000000){
+        continue;
+    }
+    largeCities[city] = worldCities[city];
+}
+console.log(largeCities);
 
-   */
 
 
 /* 
