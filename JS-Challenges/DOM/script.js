557a4cd6d6c990710eb8ff24c42269c8c78929ae
Vansh Dixit <officialvanshdixit@gmail.com>
2026-03-13 14:24:36 +0530

    add example2

--- JS-Challenges/DOM/script.js
+++ JS-Challenges/DOM/script.js
@@ -2,3 +2,10 @@
 document.getElementById("changeTextButton").addEventListener('click', () => {
     document.getElementById("myParagraph").textContent = "you are a bad girl";
 });
+
+// example 2
+document.getElementById("highlightFirstCity").addEventListener('click', () => {
+    let cities = document.getElementById("citiesList");
+    cities.firstElementChild.classList.add("highlight")
+});
+
