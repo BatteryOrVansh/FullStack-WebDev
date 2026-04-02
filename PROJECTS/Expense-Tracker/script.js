22254782cdf3592896752e888f83fb56ec58a3cd
Vansh Dixit <officialvanshdixit@gmail.com>
2026-04-02 20:37:58 +0530

    feat: add expense submission and local storage logic

--- PROJECTS/Expense-Tracker/script.js
+++ PROJECTS/Expense-Tracker/script.js
@@ -5,6 +5,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const expenseList = document.getElementById("expense-list");
   const totalAmountDisplay = document.getElementById("total-amount");
 
+  let expenses = [];
+  let totalAmount = calculateTotal();
+
+  expenseForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = expenseNameInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value.trim());
+
+    if (name !== "" && !isNaN(amount) && amount>0){
+      const newExpense = {
+        id: Date.now(),
+        name: name, 
+        amount: amount
+      };
+      expenses.push(newExpense);
+
+      saveExpensesToLocal();
+    }
+    
+  });
+
+
+  function calculateTotal() {
+
+  };
+
+  function saveExpensesToLocal(){
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  };
 
   
 
